c8b8f65126d1003e1953fff81e438b5ac6a84c89
kunfupen <kunfupen@gmail.com>
2025-04-23 14:47:41 -0700

    homework 2 commit

--- Homework/Homework 2/Homework 2.sql
+++ Homework/Homework 2/Homework 2.sql
@@ -2,9 +2,9 @@
 
 -- (a)
 SELECT COUNT(*) 
-FROM products
+FROM products;
 
-PRAGMA classic(products);
+-- productCode is the primary key.
 
 
 -- (b)
@@ -55,5 +55,89 @@ ORDER BY priceRange DESC
 LIMIT 1;
 
 -- (e)
-SELECT MSRP
+SELECT productVendor, SUM(quantityInStock * (MSRP  - buyPrice)) AS profit,
+         AVG(buyPrice) AS avgBuyPrice
 FROM products
+GROUP BY productVendor
+ORDER BY profit DESC;
+
+
+-- Question 2
+
+-- (a)
+SELECT country, COUNT(*) AS customerCount
+FROM customers
+GROUP BY country
+ORDER BY customerNumber DESC
+LIMIT 8;
+
+-- (b)
+SELECT country, COUNT(customerNumber) AS numCustomer
+FROM customers
+GROUP BY country
+WHERE numCustomer >= 5;
+
+/* Where filters the rows before they are grouped. 
+In this case we would want to use HAVING so that we can filter the grouped results.
+*/
+
+
+-- (c)
+SELECT COUNT(*) 
+FROM customers
+WHERE creditLimit > 50000
+AND country IN ('USA', 'CANADA');
+
+-- (d)
+SELECT salesRepEmployeeNumber, COUNT(DISTINCT country) AS countryCount
+FROM customers
+WHERE salesRepEmployeeNumber IS NOT NULL
+GROUP BY salesRepEmployeeNumber
+HAVING countryCount > 1;
+
+-- Question 3
+
+-- (a)
+SELECT MAX(DATE(orderDate)), MIN(DATE(orderDate))
+FROM orders
+
+-- (b)
+SELECT customerNumber, COUNT(*) AS orderCount
+FROM orders
+GROUP BY customerNumber
+ORDER BY orderCount DESC
+LIMIT 4;
+
+-- (c)
+SELECT customerNumber, DATE(orderDate) AS orderDate,
+COUNT(*) AS orderPlaced
+FROM orders
+GROUP BY customerNumber, Date(orderDate)
+ORDER BY orderPlaced DESC
+LIMIT 1;
+
+-- (d)
+SELECT COUNT(*)
+FROM orders
+WHERE status != 'Shipped'
+AND (comments LIKE '%ustomer%');
+
+
+-- Question 4
+
+-- Wrong Query
+SELECT customerNumber, country, salesRepEmployeeNumber, creditLimit
+FROM Customers
+WHERE creditLimit >= 5e4 AND country = 'USA' OR country = 'Germany' OR country = 'France'
+ORDER BY creditLimit DESC, country;
+
+/* WHERE is used wrong, 
+ORDER BY is used to sort the results after the WHERE clause has been applied.
+*/
+
+-- Correct Query
+SELECT customerNumber, country, salesRepEmployeeNumber, creditLimit
+FROM Customers
+WHERE creditLimit >= 5e4
+AND country IN ('USA', 'Germany', 'France')
+ORDER BY country  DESC, creditLimit DESC;
